39267f8ffd466783f91809d7190fbc57a9fbd6b6
Eric <ericwong@cs.cmu.edu>
2019-02-21 18:53:08 -0500

    pushed to pip with setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='projected_sinkhorn',
+    version='0.1',
+    description="A library implementing the projected sinkhorn iteration for PyTorch",
+    author='Eric Wong',
+    author_email='ericwong@cs.cmu.edu',
+    platforms=['any'],
+    license="MIT",
+    url='https://github.com/locuslab/projected_sinkhorn',
+    packages=['projected_sinkhorn'],
+    install_requires=[
+        'torch==1.0'
+    ]
+)
